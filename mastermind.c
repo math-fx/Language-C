b8ea12ea06d3cc037b517f5710f2ddb2fb7b13d8
math-fx <mathieu.floureux@gmail.com>
2023-03-10 06:40:15 +0100

    Mastermind

--- mastermind.c
+++ mastermind.c
@@ -3,113 +3,126 @@
 #include <stdlib.h>
 #include <time.h>
 
-void afficher(int tab[4]){
-    srand( time( NULL ) );
-    for(int x = 0; x < 4; x++){
-        tab[x] = 1 + rand() %6;
-        printf("%d ", tab[x]);
-    }
+void rules(){
+    printf("Bienvenue dans le jeu du Mastermind, voici les règles :\n");
+    printf("Une combinaison secrète de 4 couleurs est générée aléatoirement, vous avez 10 essais pour trouver la bonne combinaison \n");
+    printf("6 couleurs peuvent faire partie de la combinaison, les voici: \n");
+    printf("1 = Bleu, 2 = Rouge, 3 = Vert, 4 = Jaune, 5 = Orange, 6 = Noir\n");
 }
 
-void transform(int tab[4], char tab2[4]){
-
-    for(int x = 0; x < 4; x++){
-
-        if(tab[x] == 1){
-            printf("P", tab2[4]);
-        }
-
-        if(tab[x] == 2){
-            printf("B", tab2[4]);
-        }
-
-        if(tab[x] == 3){
-            printf("M", tab2[4]);
-        }
-
-        if(tab[x] == 4){
-            printf("Y", tab2[4]);
-        }
-
-        if(tab[x] == 5){
-            printf("O", tab2[4]);
-        }
-
-        if(tab[x] == 6){
-            printf("G", tab2[4]);
+void init_grille(int grille[10][9]){
+    int i, j;
+    for(i = 0; i < 10; i++){
+        for(j = 0; j < 9; j++){
+            grille[i][j] = 0;
         }
     }
 }
 
-void saisir(int tab2[4]){
-    for(int x = 0; x < 4; x++){
-        printf("Saisissez 1 chiffre compris entre 1 et 6\n ");
-        scanf("%d", &tab2[x]);
+void afficherGrille(int grille[10][9]){
+    int i, j;
+    for(i = 0; i < 10; i++){
+        for(j = 0; j < 9; j++){
+            if (j > 4){
+                if(grille[i][j] == 2){
+                    printf(".|");
+                }
+                else if(grille[i][j] == 1){
+                    printf("O|");
+                }
+                else{
+                    printf("N|");
+                }
+            }
+            else{
+                printf("|%d|", grille[i][j]);
+            }
+        }
+        printf("\n");
     }
-    printf("Voici le code\n ");
-    for (int x = 0; x < 4; x++) {
-        printf("%d", tab2[x]);
+}
+
+void code_secret(int tab[4]){
+    srand(time(NULL));
+    for(int x = 0; x < 4; x++){
+        tab[x] = 1 + rand() % 6;
     }
     printf("\n");
 }
 
-void rangement(int tab3[4]){
-    int x;
-    srand( time( NULL ) );
-    x = rand() %4;
-    for(int x=0; x<4; x++){
-        tab3[x] = 9;
-    }
-    printf("Saisissez 1 chiffre compris entre 0 et 2\n ");
-    scanf("%d", &tab3[x]);
-    printf("Voici le tableau avec la valeur :\n");
-    for(int x=0; x<4; x++){
-        if(tab3[x] == 9){
-        printf("|-|");
-        }
-        else{
-            printf("%d", tab3[x]);
-        }
-    }
-    printf("\n");
-}
-
-void valider(int tab[4]) {
-    char transform[3] = {'.', 'W', 'R'}; 
-    for(int i = 0; i < 4; i++) {
-        int choix;
+void saisir(int choix_joueur[4]){
+    printf("Saisissez 4 chiffres compris entre 1 et 6\n");
+    for (int i = 0; i < 4; i++) {
+        int chiffre;
         do {
-            printf("Saisissez 1 chiffre compris entre 0 et 2\n");
-            scanf("%d", &choix);
-        } while(choix < 0 || choix > 2);
-        tab[i] = choix;
-    }
-    for(int i = 0; i < 4; i++) {
-        tab[i] = transform[tab[i]];
+            printf("Chiffre %d : ", i + 1);
+            scanf("%1d", &chiffre);
+        } while (chiffre < 1 || chiffre > 6);
+        choix_joueur[i] = chiffre;
     }
 }
 
-int main(){    
+int main(){  
+
+    rules();
+    printf("\n");
+
+    int grille[10][9];
+    init_grille(grille);
+    printf("\n");
+
+    afficherGrille(grille);
+    printf("\n");
 
     int tab[4] = {0, 0, 0, 0};
-    afficher(tab);
+    code_secret(tab);
     printf("\n");
 
-    char tab2[4] = {0, 0, 0, 0};
-    transform(tab, tab2);
-    printf("%c %c %c %c\n", tab2[0], tab2[1], tab2[2], tab2[3]);
-    printf("\n");
+    int choix_joueur[4];
 
-    saisir(tab2);
-    printf("\n");
+    int essai = 0;
+    int code_temp[4];
+    int i = 0;
+    while(essai < 10){
+        saisir(choix_joueur);
 
-    int tab3[4] = {0, 0, 0, 0};
-    rangement(tab3);
-    
-    valider(tab3);
-    printf("\n");
-    printf("Le tableau validé est : %c %c %c %c\n", tab3[0], tab3[1], tab3[2], tab3[3]);
+        grille[essai][0] = essai + 1;
+        grille[essai][1] = choix_joueur[0];
+        grille[essai][2] = choix_joueur[1];
+        grille[essai][3] = choix_joueur[2];
+        grille[essai][4] = choix_joueur[3];
 
+        int code_temp[4] = {tab[0], tab[1], tab[2], tab[3]};
+ 
+        for(int j = 1; j < 5; j++) {
+            if (grille[essai][j] == code_temp[j-1]) {
+                code_temp[j-1] = -1;
+                grille[essai][j+4] = 2;
+            }
+        }
+
+        for(int j = 1; j < 5; j++){
+            for(int k = 0; k < 4; k++){
+                if(grille[essai][j] == code_temp[k] && grille[essai][j+4] != 2 && grille[essai][j+4] != 1){
+                    code_temp[k] = -1;
+                    grille[essai][j+4] = 1;
+                }
+            }
+        }
+
+        if (grille[essai][5]==2 && grille[essai][6]==2 && grille[essai][7]==2 && grille[essai][8]==2) {
+            essai=11;
+            afficherGrille(grille);
+            printf("GG\n");
+        }
+        else{
+            essai++;
+            afficherGrille(grille);
+        }
+        if (essai==10) {
+            printf ("Loser\n");
+        }
+    }
     system("pause");
     return 0;
 }
